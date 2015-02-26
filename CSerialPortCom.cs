1bcfe80befec58748592aaa8e2c4e8cbfd116f63
Nico Kotlenga <NicoKotlenga@live.de>
2015-02-26 18:11:43 +0100

    Klassendiagramm
    -Hinzufügen des Klassendiagramms
    -Realisierung findet morgen statt(schaffe es heute leider nicht mehr :/)
    -wichtig: Bei Receive Message bekommst du ein Message Array zurück (wie
    genau du das implementierst bleibt dir selber überlassen)
    -Bilder können verschickt werden
    -Es können nur Nachrichten zu einem aktiven User verschickt werden (die
    Einsortierung der Nachricht übernehme ich)
    -Nachdem der aktive Nutzer sich wechselt wird die Textbox zur Eingabe
    ausgegraut (kann dir morgen nochmal sagen, was genau ich mir darunter
    vorstelle)

---
+++ CSerialPortCom.cs
@@ -0,0 +1,110 @@
+﻿using System.Threading;
+using System.IO.Ports;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+class CSerialPortCOM
+{
+    private SerialPort port = new SerialPort();
+    private Thread getData;
+    private string tmpData = "";
+    private string status = "Nicht Verbunden!";
+    private string modus = "none";
+
+    public CSerialPortCOM()
+    {
+        getData = new Thread(GetData);
+        port.BaudRate = 9600;
+        port.DataBits = 8;
+        port.Parity = Parity.None;
+        port.StopBits = StopBits.One;
+        //port.ReadTimeout = 500;
+        //port.WriteTimeout = 500;
+    }
+    public bool Connect(bool handshake)
+    {
+        try
+        {
+            if (handshake) port.Handshake = Handshake.RequestToSend;
+
+            port.Open();
+            if (port.IsOpen)
+            {
+                getData.Start();
+                status = "Verbunden!";
+                if (handshake) modus = "Handshake";
+                modus = "ohne Handshake";
+                return true;
+            }
+            else
+            {
+                if (port.IsOpen) status = "Verbunden - Bereits verbunden!";
+                status = "Nicht Verbunden - Fehler beim Verbinden!";
+                return false;
+            }
+        }
+        catch
+        {
+            status = "Nicht Verbunden! - Fehler beim Verbinden!";
+            return false;
+        }
+    }
+    public bool Disconnect()
+    {
+        try
+        {
+            port.Close();
+            status = "Nicht Verbunden!";
+            modus = "none";
+            return true;
+        }
+        catch
+        {
+            if (port.IsOpen) status = "Verbunden! - Fehler beim Disconnect!";
+
+            return false;
+        }
+    }
+
+    private void GetData()
+    {
+        try
+        {
+            string line = "";
+            while ((line = port.ReadLine()) != null)
+            {
+                tmpData = line;
+            }
+        }
+        catch(Exception ex)
+        {
+            tmpData = "FEHLER beim Einlesen" + ex.Message;
+            status = "Verbunden! - FEHLER beim Einlesen!";
+        }
+    }
+    public string GetString()
+    {
+        return tmpData;
+    }
+    public bool SendString(string message)
+    {
+        try
+        {
+            port.WriteLine(message);
+            return true;
+        }
+        catch(Exception ex)
+        {
+            status = ex.Message;
+            return false;
+        }
+    }
+    public string GetStatus()
+    {
+        return status;
+    }
+}
---
+++ ChatApp.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using System.IO.Ports;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+class ChatApp
+{
+  //Attribute
+  CSerialPortCom port;
+  string[] ChatHistrory;
+  public ChatApp(CSerialPortCOM configured())
+  {
+        port = configured;
+  }
+
+
+}
